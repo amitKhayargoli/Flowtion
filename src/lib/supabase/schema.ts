bf28fb813801c1fb5442df661d955a99a67d52e8
Amit Khayargoli <khayargoliamit99@gmail.com>
2025-08-14 23:31:41 +0545

    initial schema

--- src/lib/supabase/schema.ts
+++ src/lib/supabase/schema.ts
@@ -15,5 +15,39 @@ export const workspaces = pgTable("workspaces", {
     bannerUrl: text('banner_url'),
 });
 
+export const folders = pgTable("folders", {
+        id: uuid('id').primaryKey().notNull(),
+    createdAt: timestamp('created_at', {
+        withTimezone: true,
+        mode: 'string',
+    }),
+    title: text('title').notNull(),
+    iconId: text('icon_id').notNull(),
+    data: text('data'),
+    inTrash: text('in_trash'),
+    logo: text('logo'),
+    bannerUrl: text('banner_url'),
+    workspaceId: uuid('workspace_id').references(() => workspaces.id, {
+        onDelete: 'cascade',
+    }).notNull(),
+});
 
-    
+export const files = pgTable("files", {
+        id: uuid('id').primaryKey().notNull(),
+    createdAt: timestamp('created_at', {
+        withTimezone: true,
+        mode: 'string',
+    }),
+    title: text('title').notNull(),
+    iconId: text('icon_id').notNull(),
+    data: text('data'),
+    inTrash: text('in_trash'),
+    logo: text('logo'),
+    bannerUrl: text('banner_url'),
+    workspaceId: uuid('workspace_id').references(() => workspaces.id, {
+        onDelete: 'cascade',
+    }).notNull(),
+    folderId: uuid('folder_id').references(() => folders.id, {
+        onDelete: 'cascade',
+    }).notNull(),
+});
