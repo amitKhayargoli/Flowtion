40b0a5f43b9e6a4d167e5f79ef52430dbe5dce83
Amit Khayargoli <khayargoliamit99@gmail.com>
2025-08-14 23:33:13 +0545

    relations

---
+++ migrations/relations.ts
@@ -0,0 +1,26 @@
+import { relations } from "drizzle-orm/relations";
+import { folders, files, workspaces } from "./schema";
+
+export const filesRelations = relations(files, ({one}) => ({
+	folder: one(folders, {
+		fields: [files.folderId],
+		references: [folders.id]
+	}),
+	workspace: one(workspaces, {
+		fields: [files.workspaceId],
+		references: [workspaces.id]
+	}),
+}));
+
+export const foldersRelations = relations(folders, ({one, many}) => ({
+	files: many(files),
+	workspace: one(workspaces, {
+		fields: [folders.workspaceId],
+		references: [workspaces.id]
+	}),
+}));
+
+export const workspacesRelations = relations(workspaces, ({many}) => ({
+	files: many(files),
+	folders: many(folders),
+}));
