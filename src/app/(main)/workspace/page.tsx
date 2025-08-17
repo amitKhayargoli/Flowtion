1534434eae21d55a4a54013e058bf5346c6921f3
Amit Khayargoli <khayargoliamit99@gmail.com>
2025-08-17 23:46:29 +0545

    changes

--- src/app/(main)/workspace/page.tsx
+++ src/app/(main)/workspace/page.tsx
@@ -5,7 +5,6 @@ export default async function WorkspacePage() {
     <div className="flex h-screen w-screen">
       <main className="flex-1 h-full overflow-y-auto">
         <div className="flex flex-col gap-4 p-8">
-          <h1 className="text-3xl font-bold">Workspace</h1>
         </div>
       </main>
     </div>
