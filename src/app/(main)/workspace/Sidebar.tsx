62e7c02658c55af6910bebcaacc136001ba4196a
Amit Khayargoli <khayargoliamit99@gmail.com>
2025-08-17 23:46:22 +0545

    changes

--- src/app/(main)/workspace/Sidebar.tsx
+++ src/app/(main)/workspace/Sidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { SidebarIcon } from 'lucide-react'
 import React, { useState } from 'react'
 
@@ -5,17 +7,15 @@ const Sidebar = () => {
 
   const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
 
-
   return (
-      <div className={sidebarCollapsed ? `w-64 h-full bg-white p-4`: `w-0 h-full bg-white p-4`}>
-        <span className='mb-4'>
+      <div className={!sidebarCollapsed ? `w-64 h-screen bg-[#f8f8f7] p-4 border-r-2 border-gray-200`: `w-0 h-screen bg-white p-4`}>
+        <div className='mb-4 block'>
         <h1 className='text-2xl font-bold'>
           Workspace
         </h1>
 
-        <SidebarIcon onClick={()=>setSidebarCollapsed(false)}></SidebarIcon>
-
-        </span>
+        <SidebarIcon className='cursor-pointer' onClick={()=>setSidebarCollapsed(false)}></SidebarIcon>
+        </div>
         </div>
 
 
